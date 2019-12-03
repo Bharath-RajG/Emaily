2cf1a6f5291c41d61c554e44a7000dea2a0e2be6
Bharath Raj Govindaraj <rbharath@krishservicesgroup.com>
2019-12-03 15:30:03 +0530

    Users can see survey result

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import { connect } from "react-redux";
-import * as actions from "../actions";
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-import Header from "./Header";
-import Landing from "./Landing";
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
+import Header from './Header';
+import Landing from './Landing';
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
 
 class App extends Component {
   componentDidMount() {
@@ -14,13 +14,13 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="container">
+      <div className='container'>
         <BrowserRouter>
-          <div>
+          <div className='container'>
             <Header />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/surveys" component={Dashboard} />
-            <Route path="/surveys/new" component={SurveyNew} />
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/surveys' component={Dashboard} />
+            <Route path='/surveys/new' component={SurveyNew} />
           </div>
         </BrowserRouter>
       </div>
@@ -28,7 +28,4 @@ class App extends Component {
   }
 }
 
-export default connect(
-  null,
-  actions
-)(App);
+export default connect(null, actions)(App);
